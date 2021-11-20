66e9176150bab3e2f3a65d17f9acbb02350ce7db
shixl <shixl@dist.com.cn>
2021-11-20 22:43:36 +0800

    11-20导航守卫+404页面

--- src/router/index.js
+++ src/router/index.js
@@ -4,7 +4,7 @@
  * @Autor: 史小雷
  * @Date: 2021-08-11 17:33:12
  * @LastEditors: shiXl
- * @LastEditTime: 2021-11-07 15:16:53
+ * @LastEditTime: 2021-11-20 22:41:57
  */
 import { createRouter, createWebHashHistory } from "vue-router";
 import Home from "@/components/Home.vue";
@@ -70,6 +70,14 @@ const routes = [
     component: () => import("@/views/Login.vue"),
   },
   {
+    name: "404",
+    path: "/404",
+    meta: {
+      title: "页面不存在",
+    },
+    component: () => import("@/views/404.vue"),
+  },
+  {
     ...apManagerRoute,
   },
 ];
@@ -77,5 +85,26 @@ const router = createRouter({
   history: createWebHashHistory(),
   routes,
 });
-
+/**
+ * 判断当前地址是否可以访问
+ * router.getRoutes() 获取全量路由地址
+ */
+function checkoutPermission(path) {
+  let hasPermission = router
+    .getRoutes()
+    .filter((route) => route.path == path).length;
+  if (hasPermission) {
+    return true;
+  } else {
+    return false;
+  }
+}
+router.beforeEach((to, from, next) => {
+  if (checkoutPermission(to.path)) {
+    document.title = to.meta.title;
+    next();
+  } else {
+    next("/404");
+  }
+});
 export default router;
---
+++ src/views/404.vue
@@ -0,0 +1,44 @@
+<!--
+ * @Description: 
+ * @Version: 2.0
+ * @Autor: shiXl
+ * @Date: 2021-11-20 21:47:02
+ * @LastEditors: shiXl
+ * @LastEditTime: 2021-11-20 22:24:33
+-->
+<template>
+  <div class="exception">
+    <img src="@/assets/404.jpeg" alt="" />
+    <el-button class="goHome" type="primary" @click="goHome">回首页</el-button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Exception",
+  methods: {
+    goHome() {
+      this.$router.push("/");
+    },
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+.exception {
+  width: 100%;
+  height: 100%;
+  position: relative;
+  img {
+    width: 100vw;
+    height: 100vh;
+    margin: 0;
+    padding: 0;
+  }
+  .goHome {
+    position: absolute;
+    bottom: 100px;
+    left: 50%;
+  }
+}
+</style>
